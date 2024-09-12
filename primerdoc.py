3f518ad0c94375d8903fc7e74192e07a1d38a51f
laurelcai <lvicens@uade.edu.ar>
2024-09-11 21:34:06 -0300

    Parte eventos finalizada

--- primerdoc.py
+++ primerdoc.py
@@ -1,50 +1,137 @@
 from random import randint
+import re
 
-matriz_eventos=[]
+#lista con los tipos de eventos, luego utilizada para filtrar la matriz
+lista_tipo=['musica','familia','teatro','deporte']
+
+#lista donde luego se guarda el id del evento el cual el cliente se encuentra interesado, para poder trabajar con el mismo de una manera mas agil
+lista_id=[]
+
+#matriz con todos los datos de eventos necesarios
+matriz_eventos_desordenada=[
+[1001,"musica","Milo J","Estadio de Morón","25 de octubre, 21hs","Platea  $35000","Campo  $28000",],
+[1002,"familia","Plim Plim","Quality espacio","5 de octubre, 17.30hs","Campo delantero  $17000","Campo trasero  $11000"],
+[1003,"deporte","Argentina vs Bolivia","Más Monumental","15 de Octubre, 21hs","Platea  $120000","Popular  $75000","Palco  $500000"],
+[1004,"musica","Buenos Aires Trap","Parque de la Ciudad","7 y 8 de Diciembre","Abono por un dia  $50000","Abono general  $85000"]]
 
 
-def CRUD():
+#Ordenamiento de matriz con criterio, alfabeticamente por artista/nombre
+matriz_eventos = sorted(matriz_eventos_desordenada,key=lambda x: (x[2]))
+
+def comprar(elegir1):#Ultimo paso, en el cual se efectua la confirmacion de las entradas y se elije la ubicacion en el evento
+
+    """Entrada: Se recibe el evento al cual el usuario desea asistir
+    Salida: Se muestra por pantalla los precios y ubicaciones disponibles para que el cliente confirme la compra o,
+    ya sea que quiera volver al principio, o tambien salir del programa"""
+
+    band=0
     i=0
-    tipo=0
-
-    while tipo != "SALIR":
-        tipo=input("Ingrese el tipo de evento: ")
-        tipo=tipo.upper()
-        if tipo != "SALIR":    
-            matriz_eventos.append([])
-            nombre=input("Ingrese el nombre del evento: ")
-            nombre=nombre.upper()
-            capacidad=int(input("Ingrese su capacidad total: "))
-            matriz_eventos[i].extend([tipo,nombre,capacidad])
-            i=i+1
-
-def musica():
-    filas=len(matriz_eventos)
-    columnas=len(matriz_eventos[0])
-    print(f"{'TIPO':<10} {'NOMBRE':<10} {'CAPACIDAD':>10}")
-    print('-' * 32)
-    for i in range(filas):
-         if matriz_eventos[i][0] == 'MUSICA':
-            for j in range(columnas):
-                print(f"{matriz_eventos[i][j]:<10}",end="")
-            print()
-
-
-
+    if elegir1 > 0:    
+        cont=0
+        filas=len(matriz_eventos)
+        print('-' * 100)
+        print(" "*3,f"{'UBICACION Y PRECIO':<10}")
+        print('-' * 100)
+        for i in range(filas):
+                if matriz_eventos[i][0] == lista_id[elegir1-1]:
+                    columnas=len(matriz_eventos[i])
+                    b=i
+                    for j in range(5,columnas):
+                            cont=cont+1
+                            print(cont,")",matriz_eventos[i][j])
+    band=0
+    elegir2=interfaz()
+    while band==0:
+        if elegir2 <=cont:
+            band=1
+            elegir3=int(input("Cuantas entradas desea comprar? "))
+            print("Usted ha comprado",elegir3,"entradas en la ubicacion",matriz_eventos[b][4+elegir2][:7],".")
+        else:
+            print("Ubicacion no encontrada")
+    
+    interfaz()
 
 
+def inicio():#se desplieaga un menu para seleccionar los distintos tips de eventos
+    
+    """Comienza el codigo
+    Sale: Una eleccion que deriva a la fauncion de eventos,
+    y se repite en bucle hasta no conseguir una repuesta correcta """
 
-#que se desea hacer
-CRUD()
-print('a)Música'.center(10,' '),'b)Familia'.center(10,' '),'c)Teatro'.center(10,' '),'d)Especiales'.center(10,' '))
-elegir=input("Seleccione una opción: ")
-elegir=elegir.lower()
-if elegir == "a":
-    musica()
+    band=0
+    print("Tipos de eventos: ")
+    print("-"*100)
+    print('1)Música')
+    print('2)Familia')
+    print('3)Teatro')
+    print('4)Deporte')
+    print("-"*100)
+    while band==0:
+        elegir=int(input("Seleccione una opción: "))
+        if elegir <= 4 and elegir >0:
+            eventos(elegir)
+            band=1
+        else:
+            print("Opción no encontrada")
 
-"""
-if elegir == "b":
+def interfaz():# Interfaz que se despliega luego de cada eleccion del cliente
 
-if elegir == "c":
+    """Entrada: Una eleccion por teclado
+     Salida: Lo redirije al usuario a lo seleccionado,
+     ya sea avanzar con el procedimiento, volver o incluso salir del programa en caso de arrepentimiento o equivocacion  """
 
-"""
+    elegir1=0
+    band=0
+    while band==0:
+        print('-' * 100)
+        print('0)Inicio'.center(10,' '),'-1)Salir'.center(10,' '))
+        print('-' * 100)
+        elegir1=int(input("Seleccione una opción: "))
+        if elegir1 == 0:
+            band=1
+            inicio()
+        if elegir1 == -1:
+            band=1
+            print("Adios")
+        band=1
+    
+    return elegir1
+
+
+def eventos(elegir):#Se muestran los eventos filtrados segun lo seleccionado por el usuario
+
+    """Entrada: Ingresa a la funcion como dato el tipo de evento seleccionado previamente.
+        Salida: Sale la impresion de cada evento del respectivo evento elegido previamente para asi efectuar la comora,
+        se repite en bucle hasta conseguir una respuesta adecuada"""
+
+    band=0
+    while band != 1:
+        lista_id.clear()
+        cont=0
+        filas=len(matriz_eventos)
+        print('-' * 100)
+        print(" "*3,f"{'TIPO':<20} {'NOMBRE':25} {'UBICACION':<25} {'FECHA Y HORA':<20}")
+        print('-' * 100)
+        for i in range(filas):
+            columnas=len(matriz_eventos[i])
+            if matriz_eventos[i][1] == lista_tipo[elegir-1]:
+                band=2
+                cont=cont+1
+                lista_id.append(matriz_eventos[i][0])
+                print(cont,")",end=" ")
+                print(f"{matriz_eventos[i][1]:<20} {matriz_eventos[i][2]:<25} {matriz_eventos[i][3]:<25} {matriz_eventos[i][4]:<20}")
+        if band==0:
+            print("Todavia no se encontraron eventos, intenta de nuevo mas tarde")
+            band=0
+        eleccion=interfaz()
+        if eleccion <= len(lista_id) and eleccion >0:
+            comprar(eleccion)
+            band=1
+        else:
+            if eleccion != 0 and eleccion != -1:
+                print("ERROR,vento no encontrado")
+            else:
+                band=1
+
+#menu principal
+inicio()
